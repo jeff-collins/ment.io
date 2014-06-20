3a13798b0a3452c842cd94588ae61fff3d08fef7
Jeff Collins <jeff.collins@lithium.com>
2014-06-20 16:33:56 -0700

    Hide the menu when clicking outside

--- dist/js/mentio.js
+++ dist/js/mentio.js
@@ -533,7 +533,7 @@
                     atVar: "=ngModel",
                     macros: "="
                 },
-                controller: function($scope, $timeout) {
+                controller: function($scope, $timeout, $document) {
                     this.addRule = function(rule) {
                         $scope.map[rule.triggerChar] = rule;
                         $scope.triggerCharSet.push(rule.triggerChar);
@@ -577,7 +577,15 @@
                           $timeout.cancel(timer);
                         });
                     }
-               },
+
+                    $document.on(
+                        "click", function(e) {
+                            $scope.$apply(function() {
+                                $scope.hideAll();
+                            });
+                        }
+                    );
+                },
                 link: function (scope, element, attrs, timeout) {
                     scope.map = {};
                     scope.triggerCharSet = [];
@@ -617,8 +625,8 @@
         }
     )
     .directive('mentioRule', function () {
-        var setupKeyCapture = function (scope, input) {
-            angular.element(input).bind("keydown keypress", function (event) {
+        var setupKeyCapture = function (scope, element) {
+            angular.element(element).bind("keydown keypress", function (event) {
                 if (!scope.hide) {
                     if (event.which === 27) {
                         scope.$apply(function () {
@@ -778,6 +786,7 @@
                     scope.$apply(function () {
                         controller.selector(item);
                     });
+                    e.preventDefault();
                 });
             }
         };
