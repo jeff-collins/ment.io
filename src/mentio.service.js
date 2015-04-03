be403e824b6ac2eb5d049f613db49af56afd0884
Renaud Parent <renaud.parent@gmail.com>
2015-04-03 12:08:07 +0200

    Use input font size instead of hardcoded 20px to place menu

--- src/mentio.service.js
+++ src/mentio.service.js
@@ -18,8 +18,9 @@ angular.module('mentio')
                 }
 
                 // Move the button into place.
+                var textFontSize = _getStyle(getDocument(ctx).activeElement, 'font-size').replace('px', '');
                 selectionEl.css({
-                    top: (coordinates.top - selectionEl[0].scrollHeight - 20) + 'px',
+                    top: (coordinates.top - selectionEl[0].scrollHeight - textFontSize) + 'px',
                     left: coordinates.left + 'px',
                     position: 'absolute',
                     zIndex: 100,
@@ -33,6 +34,23 @@ angular.module('mentio')
                 selectionEl.css({
                     display: 'none'
                 });
+            }
+        }
+
+        function _getStyle(el, styleProp) {
+            var camelize = function (str) {
+                return str.replace(/\-(\w)/g, function(str, letter){
+                    return letter.toUpperCase();
+                });
+            };
+
+            if (el.currentStyle) {
+                return el.currentStyle[camelize(styleProp)];
+            } else if (document.defaultView && document.defaultView.getComputedStyle) {
+                return document.defaultView.getComputedStyle(el,null)
+                                           .getPropertyValue(styleProp);
+            } else {
+                return el.style[camelize(styleProp)];
             }
         }
 
